d6ae0d9842e210506954f0d8b0dfb4b9e54f1bb9
Uzurch1k <galcenkovlad7@gmail.com>
2024-01-16 20:03:29 +0200

    fix

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-console.log('Number of categories:', document.querySelectorAll('.item').length);
-
-const categoriesItem = document.querySelectorAll('.item');
-
-categoriesItem.forEach(item => {
-  console.log(`Category: ${item.querySelector('h2').textContent}`);
-  console.log(`Elements: ${item.querySelectorAll('ul li').length}`);
-});
